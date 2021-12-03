d243e1f908bc788e44c035ef26b4b10bae2e144b
arvinrivers <90714951+arvinrivers@users.noreply.github.com>
2021-12-03 11:22:36 +0100

    more dir

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,3 +1,23 @@
+#include "../includes/GameEngine.h"
+#include <SDL2/SDL.h>
 
+System::System() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    win = SDL_CreateWindow("CoinCollector", 10, 10, 700, 500, 0);
+    ren = SDL_CreateRenderer(win, -1, 0);
+    Mix_OpenAudio(20050, AUDIO_S16SYS, 2, 4096);
+    // Path to your own 'sounds' folder!
+    //musik = Mix_LoadWAV("/Users/kjellna/dev/cpp21/f13b/sounds/bgMusic.wav");
+    music = Mix_LoadWAV( (resPath + "sounds/bgMusic.wav").c_str() );
+    Mix_PlayChannel(-1, music, -1);
+}
 
-#include "GameEngine.h"
+System::~System() {
+    Mix_FreeChunk(music);
+    Mix_CloseAudio();
+    SDL_DestroyWindow(win);
+    SDL_DestroyRenderer(ren);
+    SDL_Quit();
+}
+
+System sys;

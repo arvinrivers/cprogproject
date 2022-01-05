d0096c82f182f69db4b6a5fdda792e60aae20738
arvinrivers <90714951+arvinrivers@users.noreply.github.com>
2022-01-05 14:50:31 +0100

    pff

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include "../include/GameEngine.h"
+#include "GameEngine.h"
+#include "Sprite.h"
+#include "Player.h"
 
 int main(int argc, char* argv[]) {
     GameEngine gameEngine;
+    Player* player = new Player(0,450,50,50, "C:/Users/arsol/OneDrive/Skrivbord/cprogproject-4/resources/images/821759741633626212.png");
+    gameEngine.add(player);
     gameEngine.run();
     return 0;
 }

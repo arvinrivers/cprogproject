5e0895fe954b182a5b937b9da770981b245488d4
stjertnylle <maen0574@student.su.se>
2022-01-05 16:07:05 +0100

    filhantering

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
     GameEngine gameEngine;
-    Player* player = new Player(100,100,50,50, "C:/Users/arsol/OneDrive/Skrivbord/cprogproject-5/resources/images/821759741633626212.png");
+    Player* player = new Player(100,100,50,50, "images/821759741633626212.png");
     gameEngine.add(player);
     gameEngine.run();
     return 0;
